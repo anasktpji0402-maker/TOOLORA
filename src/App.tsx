4765fe206ecb277e13d320e77b3c005df623815d
anasktpji0402-maker <anasktpji0402@gmail.com>
2026-09-23 13:50:23 +0530

    Create App component with initial layout

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>TOOLORA is Live!</h1>
+      <p>പ്രൊജക്റ്റ് വിജയകരമായി ഡിപ്ലോയ് ചെയ്തു കഴിഞ്ഞു.</p>
+    </div>
+  );
+}
